250f77517101002d2e059d55d17a07b62df4e341
s19229 <s19229@pjwstk.edu.pl>
2020-03-06 15:29:54 +0100

    new changes

--- Tutorial1_solution/Tutorial1/Program.cs
+++ Tutorial1_solution/Tutorial1/Program.cs
@@ -9,20 +9,43 @@ namespace Tutorial1
     {
         public static async Task Main(string[] args)
         {
-            var websiteUrl = args[0];
-            var httpClient = new HttpClient();
-            var respnse = await httpClient.GetAsync(websiteUrl);
-            if(respnse.IsSuccessStatusCode)
+            var websiteUrl = args.Length > 0 ? args[0] : throw new ArgumentNullException();
+            //websiteUrl = null;
+            var x = websiteUrl ?? throw new ArgumentException("the url is null");
+            if(websiteUrl == null)
             {
-                var htmlContent = await respnse.Content.ReadAsStringAsync();
-
-                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
-                var emailAddresses = regex.Matches(htmlContent);
-                foreach (var match in emailAddresses)
-                {
-                    Console.WriteLine(match.ToString());
-                }
+                throw new ArgumentException("nah");
             }
+            var httpClient = new HttpClient();
+
+            try
+            {
+                var respnse = await httpClient.GetAsync(websiteUrl);
+                if (respnse.IsSuccessStatusCode)
+                {
+                    var htmlContent = await respnse.Content.ReadAsStringAsync();
+
+                    var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                    var emailAddresses = regex.Matches(htmlContent);
+                    if(emailAddresses.Count > 0)
+                    {
+                        foreach (var match in emailAddresses)
+                        {
+                            Console.WriteLine(match.ToString());
+                        }
+                    } else
+                    {
+                        Console.WriteLine("no email addresses found");
+                    }
+                   
+                }
+
+            } 
+            catch(Exception)
+            {
+                Console.WriteLine("error");
+            }
+          
             Console.WriteLine("dev");
         }
      
